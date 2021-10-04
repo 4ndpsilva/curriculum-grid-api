cf41fc912068ead01ad209a065dece52a7d547b6
André <4ndpsilva@gmail.com>
2021-10-04 02:24:41 -0300

    Inicial commit

---
+++ src/main/java/com/rasmoo/curriculumgrid/CurriculumGridApiApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.curriculumgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumGridApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumGridApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/curriculumgrid/CurriculumGridApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.curriculumgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumGridApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
