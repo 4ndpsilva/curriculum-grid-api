53d199dd6ae03d8de18f77b64c260230241ad00f
André Pereira da Silva <4ndpsilva@gmail.com>
2021-12-08 17:36:47 -0300

    Organização de imports

--- src/main/java/com/rasmoo/curriculumgrid/controller/MatterController.java
+++ src/main/java/com/rasmoo/curriculumgrid/controller/MatterController.java
@@ -4,11 +4,17 @@ import com.rasmoo.curriculumgrid.dto.MatterDTO;
 import com.rasmoo.curriculumgrid.entity.Matter;
 import com.rasmoo.curriculumgrid.mapper.MatterMapper;
 import com.rasmoo.curriculumgrid.service.CrudService;
-import com.rasmoo.curriculumgrid.service.MatterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
